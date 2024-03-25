e9193477faecb6e8f94dded49589be670a9ad51d
MarcelTamada <101373041+MarcelTamada@users.noreply.github.com>
2024-03-25 12:40:34 -0300

    Update movimentoplayer.cs
    Corrigido o bug de movimento quando o W e S são pressionados ao mesmo tempo

--- Assets/Scenes/scripts/movimentoplayer.cs
+++ Assets/Scenes/scripts/movimentoplayer.cs
@@ -7,45 +7,38 @@ public class movimentoplayer : MonoBehaviour
     public float velocidade = 1.5f; // Velocidade de movimento do personagem
     public float velocidadeRotacao = 100.0f; // Velocidade de rotação do personagem
     private Animator anim;
+    public bool IsGrounded;
 
     void Start()
     {
         anim = GetComponent<Animator>();
     }
 
-    // Update é chamado uma vez por frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * velocidade);
             anim.SetBool("avancar", true);
             anim.SetBool("voltar", false);
         }
-        if(Input.GetKeyUp(KeyCode.W))
-        {
-            anim.SetBool("avancar", false);
-        }
-
-
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.back * Time.deltaTime * velocidade);
             anim.SetBool("avancar", false);
             anim.SetBool("voltar", true);
         }
-        if(Input.GetKeyUp(KeyCode.S))
+        else
         {
+            anim.SetBool("avancar", false);
             anim.SetBool("voltar", false);
         }
-
 
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.up, -velocidadeRotacao * Time.deltaTime);
         }
-
 
 
         if (Input.GetKey(KeyCode.D))
